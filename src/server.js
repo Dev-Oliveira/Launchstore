986674cfc2944498dcab940230bfea6b9331d803
Hellom Oliveira <hellom-mendes@hotmail.com>
2021-06-05 03:27:56 +0100

    first commit

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require('./routes')
+const MethodOverride = require('method-override')
+const server = express()
+
+
+server.use(express.urlencoded({extended: true}))
+server.use(express.static('public'))
+server.use(MethodOverride('_method'))
+server.use(routes)
+
+
+
+server.set("view engine", "njk")
+
+nunjucks.configure("src/app/views", {
+    express: server,
+    autoescape: false,
+    noCache: true,
+    watch: true
+})
+
+
+server.listen(5000, function(err, results){
+    console.log("server is running")
+    
+})
